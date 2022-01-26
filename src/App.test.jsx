22b8b02d6e26248efe4d51789886c66a7a7a5eb5
spencer-eagleton <86867967+spencer-eagleton@users.noreply.github.com>
2022-01-26 15:27:21 -0800

    passed first test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -4,6 +4,8 @@
 import { screen, render } from '@testing-library/react'
 // 🚨
 // import rest
+import { rest } from 'msw'
+import { setupServer } from 'msw/node'
 // import setupServer
 import App from './App'
 
@@ -11,7 +13,7 @@ const user = {
   id: 1,
   created_at: '2021-12-13T00:17:29+00:00',
   // 🚨 Add a name here
-  name: '',
+  name: 'Spencer',
   avatar: 'https://thumbs.gfycat.com/NiceRequiredGrunion-size_restricted.gif',
   header: 'https://static.wikia.nocookie.net/naruto/images/5/50/Team_Kakashi.png',
   likes: ['React', 'Anime', 'Traveling', 'Living', 'Tower Defense Games', 'Card Games'],
@@ -20,14 +22,23 @@ const user = {
 }
 
 // 🚨 Create your server
+const server = setupServer(
+  rest.get('https://uzgiamkrbapxufnwdrja.supabase.co/rest/v1/users', (req, res, ctx) => {
+    const select = req.url.searchParams.get('select')
+    if (select === '*') {
+      return res(ctx.json([user]))
+    }
 
+    return res(ctx.json)
+  })
+)
 // 🚨 Listen for server start
-beforeAll()
+beforeAll(() => server.listen())
 
 // 🚨 Close server when complete
-afterAll()
+afterAll(() => server.close())
 
-test('Should render the header', async () => {
+test.only('Should render the header', async () => {
   render(<App />)
   const banner = screen.getByRole('banner')
   const headerImg = screen.getByAltText(/alchemy/i)
